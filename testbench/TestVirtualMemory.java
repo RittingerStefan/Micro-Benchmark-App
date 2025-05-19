7bb47193b86b7855406fe89c56f17d99d42a9e52
Rittinger Stefan <stefan.rittinger@bloodochallenge.com>
2025-05-19 21:15:04 +0300

    Assignment 12 - Virtual Memory completed

---
+++ testbench/TestVirtualMemory.java
@@ -0,0 +1,22 @@
+package testbench;
+
+import bench.IBenchmark;
+import bench.ram.VirtualMemoryBenchmark;
+import logging.ConsoleLogger;
+import logging.ILogger;
+
+public class TestVirtualMemory {
+    public static void main(String[] args) {
+        ILogger log = new ConsoleLogger();
+        VirtualMemoryBenchmark bench = new VirtualMemoryBenchmark();
+
+        long fileSize = 4L * 1024 * 1024 * 1024;
+        int bufferSize = 4 * 1024;
+
+        bench.run(fileSize, bufferSize);
+        log.write(bench.getResult());
+s
+        bench.clean();
+        log.close();
+    }
+}
