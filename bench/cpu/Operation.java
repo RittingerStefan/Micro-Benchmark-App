383dcf0b68224cc5adff442a5aaac17b0fcb23ea
Paul <buciumanpaul2012@gmail.com>
2025-05-21 18:14:35 +0300

    To do: use classes for timer

---
+++ bench/cpu/Operation.java
@@ -0,0 +1,5 @@
+package bench.cpu;
+
+public enum Operation {
+    IntegerArithmetic, Branch, ArrayAccess
+}
--- timing/ITimer.java
+++ timing/ITimer.java
@@ -1,31 +1,31 @@
 package timing;
 
 public interface ITimer {
-    /**
-     * Starts a new timer. <br>
-     * If called without a prior <b>stop</b>, it resets the current time, losing all 
-     * time information.
-     */
-    void start();
 
-    /**
-     * Stops and resets the current timer. Does nothing if no timer is running
-     * 
-     * @return Elapsed <b>nanoseconds</b> since last <b>start</b> was invoked.
-     */
-    long stop();
+	/**
+	 * Starts a new timer. <br>
+	 * If called without a prior <b>stop</b>, it resets the current time loosing
+	 * all time information.
+	 */
+	public void start();
 
-    /**
-     * Resumes the timer if it was previously paused. <br>
-     * If called without a prior <b>pause</b>, it does nothing. <br>
-     * If no <b>start</b> was called, it will <b>not</b> start the timer.
-     */
-    void resume();
+	/**
+	 * Stop and reset current timer.
+	 * 
+	 * @return Elapsed <b>nanoseconds</b> since <b>start</b> was invoked.
+	 */
+	public long stop();
 
-    /**
-     * Pauses the currently running timer, if there is one. Does not reset the timer.
-     * 
-     * @return Elapsed <b>nanoseconds</b> since last <b>start</b> or <b>resume</b> was invoked.
-     */
-    long pause();
+	/**
+	 * Pauses the running timer. Can be later resumed by calling <b>resume</b>.	 
+	 * @return Elapsed <b>nanoseconds</b> since <b>start</b> was invoked.
+	 */
+	public long pause();
+	
+	/**
+	 * Resumes the paused timer. <br>	 
+	 * If timer is not paused it starts it.	
+	 */
+	public void resume();
+
 }
