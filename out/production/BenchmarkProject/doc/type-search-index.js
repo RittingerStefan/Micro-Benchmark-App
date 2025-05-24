723087a10e4bacdf8231d96733270a7b85683624
Diana Rusu <dianarusu2004@gmail.com>
2025-05-24 17:02:48 +0300

    Removed out/ files

--- out/production/BenchmarkProject/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"bench","l":"IBenchmark"},{"p":"logging","l":"ILogging"},{"p":"timing","l":"ITiming"}];updateSearchResults();
